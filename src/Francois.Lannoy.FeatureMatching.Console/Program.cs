c3befb9b85a0b04126364aa4fac7b8274dbcb204
Un-dev <francois.lannoy@kelindi.com>
2022-02-08 11:54:43 +0100

    console

--- src/Francois.Lannoy.FeatureMatching.Console/Program.cs
+++ src/Francois.Lannoy.FeatureMatching.Console/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using Francois.Lannoy.FeatureMatching;
+
+    static string GetExecutingPath(string path)
+    {
+        var executingAssemblyPath =
+            Assembly.GetExecutingAssembly().Location;
+        var executingPath = Path.GetDirectoryName(executingAssemblyPath);
+        return path;
+    }
+    var executingPath = GetExecutingPath(args[0]);
+    var imageScenesData = new List<byte[]>();
+    foreach (var imagePath in
+        Directory.EnumerateFiles(Path.Combine(executingPath, args[1])))
+    {
+        var imageBytes = await File.ReadAllBytesAsync(imagePath);
+        imageScenesData.Add(imageBytes);
+    }
+    Console.WriteLine("Hello, World!");
